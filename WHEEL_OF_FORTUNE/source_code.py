6a4c49cf3a94d852a238b3a4a37b08283cb35fd9
Behterin Ahmed Biswas <behterin2000@gmail.com>
2024-02-29 10:47:59 +0530

    added phrases json file

--- WHEEL_OF_FORTUNE/source_code.py
+++ WHEEL_OF_FORTUNE/source_code.py
@@ -1,7 +1,3 @@
-
-# PASTE YOUR WOFPlayer CLASS (from part A) HERE
-# PASTE YOUR WOFHumanPlayer CLASS (from part B) HERE
-# PASTE YOUR WOFComputerPlayer CLASS (from part C) HERE
 import random
 VOWEL_COST = 250
 LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -86,8 +82,6 @@ class WOFComputerPlayer(WOFPlayer):
             else:
                 return random.choice(self.possible)
 
-import sys
-sys.setExecutionLimit(600000) # let this take up to 10 minutes
 
 import json
 import random
@@ -133,7 +127,6 @@ def spinWheel():
 def getRandomCategoryAndPhrase():
     with open("phrases.json", 'r') as f:
         phrases = json.loads(f.read())
-
         category = random.choice(list(phrases.keys()))
         phrase   = random.choice(phrases[category])
         return (category, phrase.upper())
